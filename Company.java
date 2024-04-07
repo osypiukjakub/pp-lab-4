f3fd28d485cee8b8d23812cf5864150da8fd827e
osypiukjakub <osypiuk.jakub@gmail.com>
2024-04-07 09:11:54 +0200

    zad4

--- Company.java
+++ Company.java
@@ -2,18 +2,27 @@ import Project.employees.*;
 
 public class Company {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
         employees[0] = new Manager("Zbigniew Kowalski", 70000,0);
         employees[1] = new Worker("Helena Kowalska", 20000,"Hydraulik");
         employees[2] = new Employee("Michal Nowak", 67000);
         employees[3] = new Worker("Klara Ras", 8000, "Straznik");
         employees[4] = new Worker("Fiona Bus", 9000, "Kucharz");
+        employees[5] = new Manager("Ola Rz", 70000, 0);
+        employees[6] = new Worker("Adrian Wos", 5000, "Hydraulik");
 
         System.out.println("Pracownik z indeksem 3:");
         System.out.println(employees[3]);
 
     
         employees[3].setSalary(54000);
+
+
+        for (Employee employee : employees) {
+            double newSalary = employee.getSalary() + 500;
+            employee.setSalary(newSalary);
+        }
+
 
         int niemanagerowie = 0;
 
@@ -24,6 +33,7 @@ public class Company {
         }
 
         ((Manager) employees[0]).setNumberOfSubordinates(niemanagerowie);
+        ((Manager) employees[5]).setNumberOfSubordinates(niemanagerowie);
 
         employees[0].setSalary(7500);
 
